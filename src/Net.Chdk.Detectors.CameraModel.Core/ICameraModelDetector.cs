f4cd4ad11b80e272928dd9f2ad5e172171874bc3
Dmitry Shechtman <damnian@gmail.com>
2017-04-21 16:50:38 +0300

    Add solution

---
+++ src/Net.Chdk.Detectors.CameraModel.Core/ICameraModelDetector.cs
@@ -0,0 +1,10 @@
+﻿using Net.Chdk.Model.CameraModel;
+using Net.Chdk.Model.Card;
+
+namespace Net.Chdk.Detectors.CameraModel.Core
+{
+    public interface ICameraModelDetector
+    {
+        CameraModelInfo GetCameraModel(CardInfo cardInfo);
+    }
+}
