33dd07177966e0185f2237acac56b89c7413f457
Dmitry Shechtman <damnian@gmail.com>
2017-04-21 20:44:31 +0300

    Add IFileCameraModelDetector

---
+++ src/Net.Chdk.Detectors.CameraModel.Core/IFileCameraModelDetector.cs
@@ -0,0 +1,9 @@
+﻿using Net.Chdk.Model.CameraModel;
+
+namespace Net.Chdk.Detectors.CameraModel
+{
+    public interface IFileCameraModelDetector
+    {
+        CameraModelInfo[] GetCameraModels(string filePath);
+    }
+}
