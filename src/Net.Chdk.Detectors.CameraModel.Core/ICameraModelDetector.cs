2e8695c7b40df874416832b61fd4a4a85924bd70
Dmitry Shechtman <damnian@gmail.com>
2017-05-28 15:33:00 +0300

    Add SoftwareInfo

--- src/Net.Chdk.Detectors.CameraModel.Core/ICameraModelDetector.cs
+++ src/Net.Chdk.Detectors.CameraModel.Core/ICameraModelDetector.cs
@@ -1,9 +1,10 @@
 ﻿using Net.Chdk.Model.Card;
+using Net.Chdk.Model.Software;
 
 namespace Net.Chdk.Detectors.CameraModel
 {
     public interface ICameraModelDetector
     {
-        CameraModels GetCameraModels(CardInfo cardInfo);
+        CameraModels GetCameraModels(CardInfo cardInfo, SoftwareInfo softwareInfo);
     }
 }
