bdb4b23a6a2f5e147a7ce6f7d02e8ed855f06df7
Dmitry Shechtman <damnian@gmail.com>
2017-05-31 17:25:45 +0300

    Add cancellation

--- src/Net.Chdk.Detectors.CameraModel.Core/IFileCameraModelDetector.cs
+++ src/Net.Chdk.Detectors.CameraModel.Core/IFileCameraModelDetector.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Threading;
 
 namespace Net.Chdk.Detectors.CameraModel
 {
     public interface IFileCameraModelDetector
     {
-        CameraModels GetCameraModels(string filePath, IProgress<double> progress);
+        CameraModels GetCameraModels(string filePath, IProgress<double> progress, CancellationToken token);
     }
 }
